62c71bce22355c0a689991fab49f2a26f242ce79
William Dowling <wmdowling@gmail.com>
2016-01-19 20:04:46 +0000

    reorder outb values, add licensing

--- sys/boot/io.h
+++ sys/boot/io.h
@@ -1,13 +1,23 @@
 /*
- * Code sourced from Linux kernel sys/io.h
+ * Permission to use, copy, modify, and distribute this software any
+ * purpose with or without fee is hereby granted, provided that above
+ * copyright notice and this permission notice appear copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #ifndef INCLUDE_IO_H
 #define INCLUDE_IO_H
 
 static __inline void
-outb(unsigned char data, unsigned short int port)
+outb(uint16_t port, uint16_t data)
 {
-	__asm__ __volatile__ ("outb %b0,%w1": :"a" (data), "Nd" (port));
+	__asm volatile ("outb %b0,%w1": :"a" (data), "Nd" (port));
 }
 
 #endif
