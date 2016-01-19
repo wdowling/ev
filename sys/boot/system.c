352e6b808f0251a3097ba5e30b6cf3f4a9194e36
William Dowling <wmdowling@gmail.com>
2016-01-19 20:05:27 +0000

    template for kernel level functionality

---
+++ sys/boot/system.c
@@ -0,0 +1,20 @@
+/*
+ * Permission to use, copy, modify, and distribute this software any
+ * purpose with or without fee is hereby granted, provided that above
+ * copyright notice and this permission notice appear copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <system.h>
+
+void
+__write(const char *message, size_t len)
+{
+}
