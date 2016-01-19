00346350bbe13d042670af77e8f5031fbbeffdaf
William Dowling <wmdowling@gmail.com>
2016-01-19 20:05:01 +0000

    prototype __write function

---
+++ sys/boot/system.h
@@ -0,0 +1,22 @@
+/*
+ * Permission to use, copy, modify, and distribute this software any
+ * purpose with or without fee is hereby granted, provided that above
+ * copyright notice and this permission notice appear copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifndef INCLUDE_SYSTEM_H
+#define INCLUDE_SYSTEM_H
+
+#include <stdint.h>
+
+/* Internal kernel prototypes */
+void __write(const char *, size_t);
+
+#endif
