8c1a5aa333c8eae6251e14a932d647cf43cc0cb1
William Dowling <wmdowling@gmail.com>
2016-01-28 21:11:18 +0000

    removed compilation options

--- build.sh
+++ build.sh
@@ -4,14 +4,6 @@
 # usage:
 #    ./build.sh
 #
-CROSS_PATH="/opt/cross/bin/"
-HOME="/home/wmd"
-
-cd sys/boot
-$CROSS_PATH/i686-elf-as boot.s -o boot.o
-$CROSS_PATH/i686-elf-gcc -c init_kernel.c -o init_kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-$CROSS_PATH/i686-elf-gcc -T linker.ld -o ev.bin -ffreestanding -O2 -nostdlib boot.o init_kernel.o -lgcc
-
 mkdir -p $HOME/iso/boot/grub
 cp $HOME/ev/sys/boot/ev.bin $HOME/iso/boot/ev.bin
 
