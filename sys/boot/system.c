9c60b5a0c4cc2cbb49eb27ce708c795c36769228
William Dowling <wmdowling@gmail.com>
2016-02-03 10:42:52 +0000

    Separated __write_char, __write and __strlen functions to their own system files.

--- sys/boot/system.c
+++ sys/boot/system.c
@@ -12,9 +12,46 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <system.h>
+#include "system.h"
+#include "console.h"
 
+/* __strlen() returns the length of a string. */
+size_t
+__strlen(const char *msgstring)
+{
+	size_t ret = 0;
+	while (msgstring[ret] != 0)
+		ret++;
+
+	return ret;
+}
+
+/* Set the character along with the foreground and background colours to write
+ * to the console.
+ */
+uint16_t
+__write_char(char c, uint16_t c_col)
+{
+	uint16_t c_char = c;
+	uint16_t c_color = c_col;
+	return c_char | c_color << 8;
+}
+
+/* Write the character string in *message to the console */
 void
 __write(const char *message, size_t len)
 {
+	size_t console_row = 0;
+	size_t console_column = 0;
+
+	for (size_t mc = 0; mc < len; mc++) {
+		const size_t j = console_row * CONSOLE_WIDTH + console_column;
+		framebuffer[j] = __write_char(message[mc], color);
+		if (++console_column == CONSOLE_WIDTH) {
+			console_column = 0;
+			if (++console_column == CONSOLE_HEIGHT) {
+				console_column = 0;
+			}
+		}
+	}
 }
